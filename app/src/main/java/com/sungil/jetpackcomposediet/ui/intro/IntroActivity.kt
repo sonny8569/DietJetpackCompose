dff37f5ecb87a0048170cfb6442d88615ebf4091
SUNGIL KIM <sonny0204@partron.co.kr>
2025-03-11 14:46:47 +0900

    splash 화면 설계

---
+++ app/src/main/java/com/sungil/jetpackcomposediet/ui/intro/IntroActivity.kt
@@ -0,0 +1,17 @@
+package com.sungil.jetpackcomposediet.ui.intro
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class IntroActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContent {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/sungil/jetpackcomposediet/ui/intro/IntroViewModel.kt
@@ -0,0 +1,6 @@
+package com.sungil.jetpackcomposediet.ui.intro
+
+import androidx.lifecycle.ViewModel
+
+class IntroViewModel : ViewModel() {
+}
