54183251e6b8a54125d83797baf2779cd5aaf281
SUNGIL KIM <sonny0204@partron.co.kr>
2025-03-12 17:28:18 +0900

    MainActivity 변경 로직 추가

--- app/src/main/java/com/sungil/jetpackcomposediet/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/sungil/jetpackcomposediet/ui/intro/IntroActivity.kt
@@ -1,12 +1,15 @@
 package com.sungil.jetpackcomposediet.ui.intro
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.sungil.jetpackcomposediet.MainActivity
 import com.sungil.jetpackcomposediet.ui.intro.screen.LoginScreen
 
 class IntroActivity : ComponentActivity() {
@@ -15,19 +18,28 @@ class IntroActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         setContent {
-
+            val context = LocalContext.current
             val user by viewModel.authState.collectAsStateWithLifecycle()
             val error by viewModel.errorState.collectAsStateWithLifecycle()
+            when (user) {
+                null -> {
+                    LoginScreen(
+                        onLogin = { email, password ->
+                            viewModel.login(email, password)
+                        },
+                        onSignUp = { email, password ->
+                            viewModel.singUp(email, password)
+                        },
+                        errorMessage = error
+                    )
+                }
 
-            LoginScreen(
-                onLogin = {email , password ->
-                    viewModel.login(email, password)
-                } ,
-                onSignUp = { email , password ->
-                    viewModel.singUp(email,password)
-                },
-                errorMessage = error
-            )
+                else -> {
+                    val intent = Intent(context, MainActivity::class.java)
+                    context.startActivity(intent)
+                    finish()
+                }
+            }
         }
     }
 }
